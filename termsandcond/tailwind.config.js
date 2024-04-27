9bfa6e76ec4fa1d567f618deb4d729cffb7ada3f
Anurag chhaperwal <76121882+anuragchamp@users.noreply.github.com>
2024-04-27 14:12:32 +0530

    Add files via upload
    intial commit

---
+++ termsandcond/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'blue':'#39BED7',
+      'white': '#fff',
+      'red':'#FF2121',
+      'light-grey':'#EFEFEF',
+      'dark-grey':'#A9A9A9'
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
